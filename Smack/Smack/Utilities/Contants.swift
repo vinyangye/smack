401566f4951e2f10c2c8f0292efc0c9bfaca60a2
vinyangye <30887974+vinyangye@users.noreply.github.com>
2017-12-01 22:10:57 +1100

    lesson-11

--- Smack/Smack/Utilities/Contants.swift
+++ Smack/Smack/Utilities/Contants.swift
@@ -12,8 +12,9 @@ typealias CompletionHandler = (_ Success: Bool) -> ()
 
 
 // URL Constants
-let BASE_URL = "https://vinchatapp.herokuapp.com/v1/"
+let BASE_URL = "https://vinychat.herokuapp.com/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register"
+let URL_LOGIN = "\(BASE_URL)account/login"
 
 // segues
 let TO_LOGIN = "toLogin"
@@ -24,3 +25,6 @@ let UNWIND = "unwindToChannel"
 let TOKEN_KEY = "token"
 let LOGGED_IN_KEY = "loggedIn"
 let USER_EMAIL = "userEmail"
+
+// Headers
+let HEADER = ["Content-Type": "application/json; charset=utf-8"]
