1270b61a0f42f96e94669cdc0120daa5ad1b5541
vinyangye <30887974+vinyangye@users.noreply.github.com>
2017-12-01 20:52:50 +1100

    lesson-10

---
+++ Smack/Smack/View/RoundedButton.swift
@@ -0,0 +1,31 @@
+//
+//  RoundedButton.swift
+//  Smack
+//
+//  Created by ye yang on 2017/12/1.
+//  Copyright © 2017年 ye yang. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.setupView()
+    }
+
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.setupView()
+    }
+ 
+    func setupView() {
+        self.layer.cornerRadius = cornerRadius
+    }
+}
